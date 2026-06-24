1cdc58a053770612459ba1b8d60a554c2bf1bc38
JSLegalForce <info@jslegalforce.nl>
2026-06-24 21:34:03 +0200

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,79 @@
+const ALLE_VRAGEN = [
+  {
+    thema: "Toepassingsbereik",
+    vraag: "Een vervoersfouillering kun je toepassen bij een slachtoffer, getuige of verdachte, maar uitsluitend wanneer je optreedt als opsporingsambtenaar op grond van het Wetboek van Strafvordering.",
+    opties: [
+      "Deze stelling klopt niet",
+      "Deze stelling klopt"
+    ],
+    juist: 0,
+    uitleg: "Een vervoersfouillering mag worden toegepast op een te vervoeren persoon. Hiervoor hoef je niet bezig te zijn met je taak als opsporingsambtenaar. Het betreft een politiebevoegdheid die samenhangt met het veilig vervoeren van personen.",
+    artikel: "Vervoersfouillering - reikwijdte"
+  },
+  {
+    thema: "Proportionaliteit",
+    vraag: "Een vervoersfouillering mag te allen tijde worden toegepast op een persoon die wordt vervoerd.",
+    opties: [
+      "Deze stelling klopt",
+      "Deze stelling klopt niet"
+    ],
+    juist: 1,
+    uitleg: "Als ambtenaar moet je deze bevoegdheid op een redelijke en gematigde wijze toepassen en altijd afwegen of een vervoersfouillering proportioneel is. Volgens artikel 7, lid 7 van de Politiewet 2012 moet iedere politiebevoegdheid op een redelijke en gematigde wijze worden uitgeoefend. Een vervoersfouillering mag daarom niet standaard worden uitgevoerd, maar moet altijd worden beoordeeld aan de hand van de omstandigheden van het geval.",
+    artikel: "Art. 7 lid 7 Politiewet 2012"
+  },
+  {
+    thema: "Wijze van onderzoek",
+    vraag: "Bij een vervoersfouillering mag je de te vervoeren persoon aan de kleding onderzoeken. Daarom mag je ook in de kleding kijken of fouilleren.",
+    opties: [
+      "Deze stelling klopt niet",
+      "Deze stelling klopt"
+    ],
+    juist: 0,
+    uitleg: "Bij een vervoersfouillering mag je de kleding slechts oppervlakkig aftasten (artikel 21, lid 1 Ambtsinstructie). Het is niet toegestaan om in de kleding te zoeken of te fouilleren. Voel je tijdens het aftasten een gevaarlijk voorwerp, dan mag je dit uiteraard uit de kleding halen.",
+    artikel: "Art. 21 lid 1 Ambtsinstructie"
+  },
+  {
+    thema: "Aangetroffen voorwerpen",
+    vraag: "Als je tijdens een vervoersfouillering een scherp voorwerp aantreft, moet je dit in beslag nemen.",
+    opties: [
+      "Deze stelling klopt",
+      "Deze stelling klopt niet"
+    ],
+    juist: 1,
+    uitleg: "Wanneer een gevaarlijk voorwerp wordt aangetroffen dat niet strafrechtelijk in beslag kan worden genomen omdat het geen verboden wapen betreft, mag dit tijdelijk in bewaring worden genomen op grond van artikel 20, lid 2 van de Ambtsinstructie.",
+    artikel: "Art. 20 lid 2 Ambtsinstructie"
+  },
+  {
+    thema: "Doel van de fouillering",
+    vraag: "Het doel van een vervoersfouillering is het opsporen en in beslag nemen van verboden wapens.",
+    opties: [
+      "Deze stelling klopt niet",
+      "Deze stelling klopt"
+    ],
+    juist: 0,
+    uitleg: "Het doel van een vervoersfouillering is om te onderzoeken of de te vervoeren persoon gevaarlijke voorwerpen bij zich draagt. Het doel is niet specifiek gericht op het opsporen en in beslag nemen van verboden wapens.",
+    artikel: "Doel vervoersfouillering"
+  },
+  {
+    thema: "Meldplicht",
+    vraag: "Een vervoersfouillering moet onverwijld schriftelijk worden gemeld aan de hulpofficier van justitie, inclusief de redenen die tot de fouillering hebben geleid.",
+    opties: [
+      "Deze stelling klopt",
+      "Deze stelling klopt niet"
+    ],
+    juist: 1,
+    uitleg: "In tegenstelling tot een veiligheidsfouillering hoeft een vervoersfouillering niet te worden gemeld aan de meerdere of hulpofficier van justitie.",
+    artikel: "Vervoersfouillering - geen meldplicht"
+  },
+  {
+    thema: "Inbeslagname en BOA Domein I",
+    vraag: "Als BOA Domein I mag ik een tijdens een vervoersfouillering aangetroffen verboden wapen, zoals een vlindermes of een boksbeugel, niet in beslag nemen.",
+    opties: [
+      "Deze stelling klopt",
+      "Deze stelling klopt niet"
+    ],
+    juist: 0,
+    uitleg: "Als boa domein I ben je niet bevoegd om strafbare wapens zoals een vlindermes of boksbeugel op grond van de Wet wapens en munitie in beslag te nemen. Je bent immers niet bevoegd voor deze wetgeving. Wel kun je dergelijke voorwerpen tijdelijk in bewaring nemen op grond van artikel 20, lid 2 van de Ambtsinstructie. Vervolgens kunnen de wapens worden overgedragen aan de politie, die bevoegd is deze strafrechtelijk in beslag te nemen.",
+    artikel: "Art. 20 lid 2 Ambtsinstructie / Wet wapens en munitie"
+  }
+];
